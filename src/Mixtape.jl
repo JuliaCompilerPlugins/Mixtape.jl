e481fb339ce48d76a33b0dbb865d499f8a6c583c
McCoy R. Becker <mccoybecker@gmail.com>
2020-06-13 15:18:45 -0700

    Explorations...

--- src/Mixtape.jl
+++ src/Mixtape.jl
@@ -2,22 +2,13 @@ module Mixtape
 
 using ExportAll
 
-using TimerOutputs
-using DataStructures
-using LLVM
-using LLVM.Interop
-using Libdl
 using Core.Compiler
 using Core.Compiler: MethodInstance, NativeInterpreter, CodeInfo, VarTable, AbstractInterpreter
-using Base.Meta
 
 import Core.Compiler: InferenceParams, OptimizationParams, get_world_counter, get_inference_cache, InferenceResult, _methods_by_ftype, OptimizationState, CodeInstance, Const, widenconst, isconstType, abstract_call_gf_by_type, abstract_call, code_cache, WorldView, lock_mi_inference, unlock_mi_inference, InferenceState
 
 
 include("mixer.jl")
-
-timings() = (TimerOutputs.print_timer(to); println())
-enable_timings() = (TimerOutputs.enable_debug_timings(Mixtape); return)
 
 end # module
 

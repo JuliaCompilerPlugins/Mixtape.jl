9ece70cff85350984e7327b6a3a1b29d49e38d2a
McCoy R. Becker <mccoybecker@gmail.com>
2020-06-30 17:15:56 -0700

    Other issues with  functions. May need to turn to a whitelist approach encoded as Val in MixTable instances.

--- src/Mixtape.jl
+++ src/Mixtape.jl
@@ -27,22 +27,16 @@ function _apply_iterate_fix(ex::Expr, new)
     #return Expr(:call, GlobalRef(@__MODULE__, :remix!), new, GlobalRef(Core, :_apply_iterate), ex.args[2:end]...)
     return ex
 end
-function core_check(ex::Expr)
-    ex.args[1] isa GlobalRef && (ex.args[1].mod == Core || parentmodule(ex.args[1].mod) == Core)
-end
-function base_check(ex::Expr)
-    ex.args[1] isa GlobalRef && (ex.args[1].mod == Base || parentmodule(ex.args[1].mod) == Base)
-end
 
 # Recursively wraps function calls with the below generated function call and inserts the context argument.
-function pipe_transform!(pr::IRTools.Pipe, hi::Type{NoHooks})
+function pipe_transform!(pr::IRTools.Pipe, hi::Type{NoHooks}; whitelist = [:rand])
     new = argument!(pr)
     for (v, st) in pr
         ex = st.expr
         if ex isa Expr && ex.head == :call
             if _apply_iterate_check(ex)
                 pr[v] = _apply_iterate_fix(ex, new)
-            elseif ex.args[1] isa GlobalRef && !core_check(ex) && !base_check(ex)
+            elseif ex.args[1] isa GlobalRef && ex.args[1].name in whitelist
                 ref = GlobalRef(@__MODULE__, :remix!)
                 pr[v] = Expr(:call, ref, new, ex.args...)
             end
@@ -50,14 +44,14 @@ function pipe_transform!(pr::IRTools.Pipe, hi::Type{NoHooks})
     end
 end
 
-function pipe_transform!(pr::IRTools.Pipe, hi::Type{Hooks})
+function pipe_transform!(pr::IRTools.Pipe, hi::Type{Hooks}; whitelist = [:rand])
     new = argument!(pr)
     for (v, st) in pr
         ex = st.expr
         if ex isa Expr && ex.head == :call
             if _apply_iterate_check(ex)
                 pr[v] = _apply_iterate_fix(ex)
-            elseif ex.args[1] isa GlobalRef && !core_check(ex) && !base_check(ex)
+            elseif ex.args[1] isa GlobalRef && ex.args[1].name in whitelist
                 insert!(pr, v, Expr(:call, GlobalRef(@__MODULE__, :scrub!), new, ex.args...))
                 insertafter!(pr, v, Expr(:call, GlobalRef(@__MODULE__, :dub!), new, ex.args...))
                 pr[v] = Expr(:call, GlobalRef(@__MODULE__, :remix!), new, ex.args...)

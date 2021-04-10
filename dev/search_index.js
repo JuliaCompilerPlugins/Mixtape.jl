9546219659ee743bf071a45f899c93d6757120a3
Documenter.jl <documenter@juliadocs.github.io>
2021-04-10 02:26:00 +0000

    build based on 160888e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#API-Documentation","page":"API Documentation","title":"API Documentation","text":"","category":"section"},{"location":"","page":"API Documentation","title":"API Documentation","text":"Below is the API documentation for Mixtape.jl","category":"page"},{"location":"","page":"API Documentation","title":"API Documentation","text":"CurrentModule = Mixtape","category":"page"}]
+}
